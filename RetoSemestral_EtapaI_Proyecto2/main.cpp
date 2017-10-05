c80454a814b8995408c9bd9391c7af07a248c1e4
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2017-10-05 10:17:40 -0500

    Adding the information
    adding my personal info to the code and the instructions needed to complete this work.

--- RetoSemestral_EtapaI_Proyecto2/main.cpp
+++ RetoSemestral_EtapaI_Proyecto2/main.cpp
@@ -1,10 +1,11 @@
-//
-//  main.cpp
-//  RetoSemestral_EtapaI_Proyecto2
-//
-//  Created by Angel Trevino on 10/5/17.
-//  Copyright © 2017 Angel Odiel Treviño Villanueva. All rights reserved.
-//
+/*
+ Angel Odiel Treviño Villanueva
+ A01336559
+ Estructura de Datos
+ Reto Semestral
+ Etapa I
+ Reto Super Washing Machines
+ */
 
 #include <iostream>
 
